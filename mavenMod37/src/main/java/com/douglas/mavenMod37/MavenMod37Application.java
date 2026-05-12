9003f3e3a2c7901741fa3429aec73eaaf767f67e
Douglas_Michelini <douglasmsrodrigues@hotmail.com>
2026-05-12 12:35:19 -0300

    first commit

---
+++ mavenMod37/src/main/java/com/douglas/mavenMod37/MavenMod37Application.java
@@ -0,0 +1,13 @@
+package com.douglas.mavenMod37;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenMod37Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenMod37Application.class, args);
+	}
+
+}
---
+++ mavenMod37/src/test/java/com/douglas/mavenMod37/MavenMod37ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglas.mavenMod37;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenMod37ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
